725ff7254a34b1d2c89ac3b4805b45a803053842
Dustboy743 <dsaidns@gmail.com>
2025-05-16 00:19:58 +0530

    with arguments

---
+++ arguments.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+value1=$1
+value2=$2
+
+echo "$value1:: Hi $value2, ssup?"
+echo "$value2:: Hey bud! Nothing..you?"
+echo "$value1:: good..hows the spacex?"
+echo "$value2:: it's good"

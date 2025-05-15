93b0d56fcff61ddb0b5f4e9430731d6eac146747
Dustboy743 <dsaidns@gmail.com>
2025-05-16 01:02:24 +0530

    addition of two numbers

--- array.sh
+++ array.sh
@@ -5,5 +5,5 @@ movies=("hello" "laila" "king")
 echo "first movie is ${movies[0]}"
 echo "second movie is ${movies[1]}"
 echo "all movies is ${movies[@]}"
-#echo "out of range movies is ${movies[4]}"
+echo "out of range movies is ${movies[4]}"
 echo "lenght of movies is ${#movies[@]}"

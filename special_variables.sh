93209ee9d34284e1f32c194acfab87a9147d022e
Dustboy743 <dsaidns@gmail.com>
2025-05-16 17:04:47 +0530

    special characters

--- special_variables.sh
+++ special_variables.sh
@@ -4,6 +4,7 @@ echo "special character for current script name:: $0"
 echo "special character for current directory:: $PWD"
 echo "special character for exit status :: $?"
 echo "special character for current process ID:: $$"
+sleep 10 &
 echo "special character for previous PID:: $!"
 echo "special character for current user:: $USER"
 echo "special character for home directory:: $HOME"

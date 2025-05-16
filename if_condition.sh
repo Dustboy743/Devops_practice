fcd53b89dc65a3fcf36cf0d0d69a9f282fb826d2
Dustboy743 <dsaidns@gmail.com>
2025-05-16 20:33:37 +0530

    if condition

--- if_condition.sh
+++ if_condition.sh
@@ -2,9 +2,9 @@
 
 number=$1
 
-if [ "$number" -gt 10 ]
+if [ "$number" -ge 10 ]
 then
-    echo "$number is greater than 10"
+    echo "$number is greater or equal to 10"
 else
     echo "$number is less than 10"    
 fi    

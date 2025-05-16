1c15dd52bf3a0ceb9b5f65f815a0fbe7d9415b04
Dustboy743 <dsaidns@gmail.com>
2025-05-16 23:01:45 +0530

    install service

--- install_service.sh
+++ install_service.sh
@@ -2,10 +2,27 @@
 
 user_id=$(id -u)  #to get the user id of the user
 
-if [ $user_id -eq 0 ]
+if [ $user_id -eq 0 ]  #check whether user is root
 then
     echo "You're the root user.."
 else
     echo "You're not the root user. Switch to root user"
     exit 1
 fi
+
+apt list --installed | grep nginx   #to check whether already installed or not
+
+if [ $? -ne 0 ]    #if installed
+then
+    echo "not installed..will start installing it"
+    apt install nginx -y
+    if [ $? -eq 0 ]    #status after installing
+    then
+        echo "great..installed it..SUCCESS"
+    else
+        echo "cannot be installed..FAILURE"
+        exit 1
+    fi
+else
+    echo "Already installed.."
+fi        

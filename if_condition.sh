57ebad0dd6386d9c526aa0f4a8d5522b402f5503
Dustboy743 <dsaidns@gmail.com>
2025-05-16 21:30:31 +0530

    install service

--- if_condition.sh
+++ if_condition.sh
@@ -4,7 +4,7 @@ number=$1
 
 if [ "$number" -ge 10 ]
 then
-echo "$number is greater or equal to 10"
+    echo "$number is greater or equal to 10"
 else
-echo "$number is less than 10"    
+    echo "$number is less than 10"    
 fi    
